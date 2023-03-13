7f56784d7941fc5b9d955f4d2526350deae271eb
Jordan Del Pilar <redjordan123@gmail.com>
2023-03-13 14:59:30 -0700

    Page Title Update
    Added javascript to change the title of the page based on the search term of the card being viewed.
    Also cleaned up the code a bit removing un-needed whitespace

--- pages/views.py
+++ pages/views.py
@@ -12,5 +12,8 @@ def Results(request):
     if request.GET['search'] != '':
         query = 'name:\"%s\"' % request.GET['search']
         cards = Card.where(q=query, orderBy='+set.releaseDate')
-        context = {'cards' : cards}
+        context = {
+            'cards' : cards,
+            'search' : request.GET['search']
+        }
     return render(request, 'pages/results.html', context)
